9176fd76d029d8d12575f4ad482c87c26984f523
Brian Carlsen <carlsen.bri@gmail.com>
2026-07-21 06:40:14 +0200

    visibility toggle switch

--- web/frontend/src/components/Visibility.tsx
+++ web/frontend/src/components/Visibility.tsx
@@ -1,5 +1,7 @@
 import Icon from "@/icon";
 import { VisibilityPrivate, VisibilityPublic, type Visibility } from "@/types";
+import classNames from "classnames";
+import { useState } from "react";
 
 interface VisibilityIconProps {
     visibility: Visibility;
@@ -11,4 +13,62 @@ export function VisibilityIcon({ visibility }: VisibilityIconProps) {
         case VisibilityPublic:
             return <Icon.Eye title="Public" />;
     }
+}
+
+interface VisibilityFormToggleProps {
+    defaultValue: Visibility;
+    className?: string;
+}
+export function VisibilityFormToggle({
+    defaultValue,
+    className,
+}: VisibilityFormToggleProps) {
+    const iconClass =
+        "text-syre-grey-600 dark:text-syre-grey-300 \
+        peer-checked:bg-primary-700 dark:peer-checked:bg-primary-500 \
+        peer-checked:text-white dark:peer-checked:text-black \
+        rounded-full block \
+        h-full aspect-square p-1 cursor-pointer";
+
+    let parentClass = "flex";
+    if (className) {
+        parentClass += " " + className;
+    }
+
+    return (
+        <fieldset className={parentClass}>
+            <label
+                className="rounded-l-full border-l border-t border-b pr-0.5 cursor-pointer"
+                title="Public"
+            >
+                <input
+                    type="radio"
+                    name="visibility"
+                    value={VisibilityPublic}
+                    defaultChecked={defaultValue === VisibilityPublic}
+                    className="hidden peer"
+                />
+                <span className="sr-only">Public</span>
+                <span className={iconClass}>
+                    <Icon.Eye />
+                </span>
+            </label>
+            <label
+                className="rounded-r-full border-r border-t border-b pl-0.5 cursor-pointer"
+                title="Private"
+            >
+                <input
+                    type="radio"
+                    name="visibility"
+                    value={VisibilityPrivate}
+                    defaultChecked={defaultValue === VisibilityPrivate}
+                    className="hidden peer"
+                />
+                <span className="sr-only">Private</span>
+                <span className={iconClass}>
+                    <Icon.EyeSlash />
+                </span>
+            </label>
+        </fieldset>
+    );
 }
